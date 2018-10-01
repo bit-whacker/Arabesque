eed54311bfed029c5f021867cedac689110ec362
Suhas <suhask229@gmail.com>
2018-10-01 00:14:49 -0400

    Remove hardcoded path. Script now depends on ARABESQUE_HOME variable being set. Check if HDFS is up. Option to pause spark-job for remote debug

--- scripts/run_qfrag_spark.sh
+++ scripts/run_qfrag_spark.sh
@@ -1,17 +1,33 @@
 #! /usr/bin/env sh
 
-# garantees that the environment knows Spark
+# guarantees that the environment knows Spark
 if [ -z "$SPARK_HOME" ]; then
    echo "Please, make sure SPARK_HOME is properly set"
    exit
 fi
 
+if [ -z "$ARABESQUE_HOME" ]; then
+   echo "Make sure ARABESQUE_HOME is properly set. Don't forget to Run arabesque-env.sh script"
+   exit
+fi
+
+# TODO : Need a more robust check to declare whether HDFS is up and running as required
+if hdfs dfsadmin -report 2>&1 >/dev/null | grep -q 'exception:'; then
+   echo "Make sure HDFS is running"
+   exit
+fi
+
 echo "Spark installation: SPARK_HOME=$SPARK_HOME"
+
+configs="$@"
+if [ $1 = "DEBUG" ]; then
+   configs="${@:2}"
+fi
 
 # configuration files in spark must be read from $SPARK_HOME/io.arabesque.conf
 # we aggregate the configs passed by the user in one single temporary file
 tempfile=$(mktemp $SPARK_HOME/conf/qfrag-yaml.XXXXXX)
-for config_file in "$@"; do
+for config_file in $configs; do
    cat $config_file >> $tempfile
 done
 
@@ -35,19 +51,20 @@ while IFS='' read -r line || [ -n "$line" ]; do
 done < "$tempfile"
 
 # qfrag executable
-QFRAG_JAR_DIR="/Users/ambermadvariya/src/Arabesque/target/"
-#QFRAG_JAR_DIR="."
-PWD="`pwd`"
-#QFRAG_JAR_DIR="$(dirname "$PWD")"
-QFRAG_JAR=`find $QFRAG_JAR_DIR -maxdepth 2 -name "*-jar-with-dependencies.jar" | head -1`
+ARABESQUE_JAR=`find $ARABESQUE_HOME -maxdepth 2 -name "arabesque*-jar-with-dependencies.jar" | head -1`
 
-if [ -z "$QFRAG_JAR" ] ; then
-  echo "No QFrag jar found in $QFRAG_JAR_DIR. Did you compile it?"
+if [ -z "$ARABESQUE_JAR" ] ; then
+  echo "No Arabesque jar found. Did you compile it?"
   exit 66
 fi
 
-# submit the application to spark cluster
-$SPARK_HOME/bin/spark-submit --driver-memory $driverMemory --conf spark.driver.maxResultSize=$maxResultSize --verbose --class  io.arabesque.QfragRunner $QFRAG_JAR -y $(basename $tempfile)
+# submit the application to spark cluster. 
+# If the DEBUG option is passed then the spark-job will pause, waiting for a remote debugger to attach
+if [ $1 = "DEBUG" ] ; then
+    $SPARK_HOME/bin/spark-submit --driver-memory $driverMemory --conf spark.driver.extraJavaOptions=-agentlib:jdwp=transport=dt_socket,server=y,suspend=y,address=5005 spark.driver.maxResultSize=$maxResultSize --verbose --class io.arabesque.QfragRunner $ARABESQUE_JAR -y $(basename $tempfile)
+else
+    $SPARK_HOME/bin/spark-submit --driver-memory $driverMemory --conf spark.driver.maxResultSize=$maxResultSize --verbose --class  io.arabesque.QfragRunner $ARABESQUE_JAR -y $(basename $tempfile)
+fi
 
 # remove the tempfile
 rm $tempfile
